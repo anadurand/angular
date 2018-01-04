5c3a1c62bb65dabf344e60ffb22017a6f7fa79cc
Ana Durand <ana@juntoz.com>
2018-01-04 18:28:07 -0500

    start new game

--- src/app/games.service.ts
+++ src/app/games.service.ts
@@ -6,20 +6,19 @@ import {of} from "rxjs/observable/of";
 
 @Injectable()
 export class GamesService {
-  games: Game[];
   gamestatus: any;
 
   constructor(private db: AngularFirestore) {
-    this.db.collection('games').valueChanges().subscribe((games: any)=> this.games = games);
     this.db.collection('gamestatus').valueChanges().subscribe((gamestatus: any)=> this.gamestatus = gamestatus);
 
   }
   getGames(): Observable<any> {
-    return this.db.collection('games').valueChanges();
+    return this.db.collection('games', ref => ref.orderBy('id')).valueChanges();
   }
   getGame(id: number): Observable<any> {
     return this.db.collection('games', ref => ref.where('id' , '==' , id)).valueChanges();
   }
+
 
 
 
