789443a6d194fd93ced5fa02270b5da65cb116e8
Ana Durand <ana@juntoz.com>
2018-01-08 12:29:27 -0500

    game status new game component

--- src/app/new-game/new-game.component.ts
+++ src/app/new-game/new-game.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {GameStatus} from "../game";
 import {GamesService} from "../games.service";
 
@@ -8,12 +8,12 @@ import {GamesService} from "../games.service";
   styleUrls: ['./new-game.component.css']
 })
 export class NewGameComponent implements OnInit {
-
+  @Input() gameStatus: GameStatus;
 
   constructor(private gamesService: GamesService) { }
 
   ngOnInit() {
-
+    console.log(this.gameStatus);
 
   }
 
